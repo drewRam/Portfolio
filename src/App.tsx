6f730d82e52438c16b6dd4882909f205e41a760c
Andrew <ars3nx@gmail.com>
2025-08-13 15:30:51 -0700

    Clean up unused files. Refactored project.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import './App.css';
+import HomeScreen from './pages/HomeScreen';
+
+const App = () => {
+  return (
+    <>
+      <HomeScreen></HomeScreen>
+    </>
+  );
+}
+
+export default App;
